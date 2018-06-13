55dfd5204a3f4c8fce78fd523eb28e6b39ff1815
Jacob Zhou <839985914@qq.com>
2018-06-13 12:59:45 +0800

    delete idea file

--- PicCut.py
+++ PicCut.py
@@ -1,48 +1,56 @@
-__author__ ='Jacob Zhou'
+__author__ = 'Jacob Zhou'
 
 from PIL import Image
 import os
 from scan_photo import getFileName
 
-#将图片填充为正方形
+# 将图片填充为正方形
+
+
 def fill_image(image):
-    width,height = image.size
-    #选取长和宽中较大值作为新图片的边长
+    width, height = image.size
+    # 选取长和宽中较大值作为新图片的边长
     new_image_length = width if width > height else height
-    #生成新白底图片
-    new_image = Image.new(image.mode,(new_image_length,new_image_length),color="white")
-    #原图居中贴在新图上
-    if width > height :
-         new_image.paste(image,(0,int((new_image_length - height) / 2)))
+    # 生成新白底图片
+    new_image = Image.new(image.mode, (new_image_length,
+                                       new_image_length), color="white")
+    # 原图居中贴在新图上
+    if width > height:
+        new_image.paste(image, (0, int((new_image_length - height) / 2)))
     else:
-         new_image.paste(image,(int((new_image_length - width) / 2),0))
+        new_image.paste(image, (int((new_image_length - width) / 2), 0))
     return new_image
-#切图
+# 切图
+
+
 def cut_image(image):
-    width,height = image.size
+    width, height = image.size
     item_width = int(width / 3)
     box_list = []
-    for i in range(0,3):
-        for j in range(0,3):
-            box = (j * item_width,i * item_width,(j + 1) * item_width,(i + 1) * item_width)
+    for i in range(0, 3):
+        for j in range(0, 3):
+            box = (j * item_width, i * item_width, (j + 1)
+                   * item_width, (i + 1) * item_width)
             box_list.append(box)
     image_list = [image.crop(box)for box in box_list]
     return image_list
-#保存
+# 保存
+
+
 def save_images(image_list):
     index = 1
-    #检查目标文件是否存在
+    # 检查目标文件是否存在
     if(os.path.isdir('result') == 0):
         os.mkdir('result')
-    #输出文件，并在控制台显示进度
+    # 输出文件，并在控制台显示进度
     for image in image_list:
-        image.save('./result/Pic' + str(index) + '.png','PNG')
+        image.save('./result/Pic' + str(index) + '.png', 'PNG')
         print(str(index * 10) + '% done')
         index += 1
 
 
 if __name__ == '__main__':
-    path= '.\\'
+    path = '.\\'
     file_path = str(path) + '\\' + str(getFileName(path))
     image = Image.open(file_path)
     image = fill_image(image)
--- scan_photo.py
+++ scan_photo.py
@@ -1,11 +1,13 @@
-__author__='Jacob Zhou'
-#scan all photos in designated file
+__author__ = 'Jacob Zhou'
+# scan all photos in designated file
 import os
 import re
-#获取指定目录下的所有图片文件的文件名
+# 获取指定目录下的所有图片文件的文件名
+
+
 def getFileName(path):
     file_list = os.listdir(path)
-    #print file_list
+    # print file_list
     for i in file_list:
         if re.match(".jpg", os.path.splitext(i)[1], re.I) or re.match(".png", os.path.splitext(i)[1], re.I):
             if __name__ == '__main__':
@@ -14,7 +16,6 @@ def getFileName(path):
                 return i
 
 
-#os.path.splitext(i)[1] == '.jpg' or os.path.splitext(i)[1] == '.JPG' or os.path.splitext(i)[1] == '.png' or os.path.splitext(i)[1] == '.PNG'
 if __name__ == '__main__':
     path = 'C:\\Users\zzh83\Desktop'
     getFileName(path)
